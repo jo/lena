6ca2766f8285273a9e1108927b9aebf81baf8b0d
Johannes J. Schmidt <schmidt@netzmerk.com>
2012-03-22 08:44:28 +0100

    render show on folder with one item

--- lena/app/views/folder.js
+++ lena/app/views/folder.js
@@ -4,19 +4,33 @@ Lena.views.Folder = Backbone.View.extend({
     this.session = options.session;
     this.ddoc = options.ddoc;
 
-    this.subviews = {
-      toolbar: new Lena.views.Toolbar({
-        el: '#toolbar',
-        model: this.session
+    this.contentViews = {
+      show: new Lena.views.Show({
+        ddoc: this.ddoc,
+        router: this.router,
+        collection: this.collection
       }),
-      content: new Lena.views.List({
+      list: new Lena.views.List({
         ddoc: this.ddoc,
         router: this.router,
         collection: this.collection
       })
     };
+
+    this.subviews = {
+      toolbar: new Lena.views.Toolbar({
+        el: '#toolbar',
+        model: this.session
+      })
+    };
     
     this.session.on('change:userCtx', this.render, this);
     this.collection.on('reset', this.render, this);
+  },
+
+  render: function() {
+    this.subviews.content = this.contentViews[this.collection.length === 1 ? 'show' : 'list'];
+
+    return Backbone.View.prototype.render.call(this);
   }
 });
